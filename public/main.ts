f5e707f86a3164d458642e3049b5413654eb5143
JamesGaier <james.gaier003@gmail.com>
2020-08-28 14:14:41 -0700

    first commit

--- public/main.ts
+++ public/main.ts
@@ -1,48 +1,48 @@
 let timer = 256;
 const tickRate = 16;
 const visualRate = 256;
-const resources = {"gold":0,"pickaxe":1};
-const costs = {"pickaxe":15,
-    "miner":200,
-    "miner_pickaxe":15};
-const growthRate = {"pickaxe":1.25,
-    "miner":1.25,
-    "miner_pickaxe":1.75};
+const resources = {gold:0,pickaxe:1};
+const costs = {pickaxe:15,
+    miner:200,
+    miner_pickaxe:15};
+const growthRate = {pickaxe:1.25,
+    miner:1.25,
+    miner_pickaxe:1.75};
 
-const increments = [{"input":["miner","miner_pickaxe"],
-    "output":"gold"}];
+const increments = [{input:["miner","miner_pickaxe"],
+    output:"gold"}];
 
-const unlocks = {"pickaxe":{"gold":10},
-    "miner":{"gold":100},
-    "miner_pickaxe":{"miner":1}};
+const unlocks = {pickaxe:{gold:10},
+    miner:{gold:100},
+    miner_pickaxe:{miner:1}};
 
-function mineGold(num: number){
+function mineGold(num: number) {
     resources["gold"] += num * resources["pickaxe"];
     updateText();
 }
 
-function upgradeMinerPickaxe(num: number){
+function upgradeMinerPickaxe(num: number) {
     if (resources["gold"] >= costs["miner_pickaxe"] * num){
         resources["miner_pickaxe"] += num;
         resources["gold"] -= num * costs["miner_pickaxe"];
-    
+
         costs["miner_pickaxe"] *= growthRate["miner_pickaxe"];
-    
+
         updateText();
     }
 }
 
-function upgradePickaxe(num){
+function upgradePickaxe(num: number) {
     if (resources["gold"] >= costs["pickaxe"] * num){
         resources["pickaxe"] += num;
         resources["gold"] -= num * costs["pickaxe"];
-    
+
         costs["pickaxe"] *= growthRate["pickaxe"];
-    
+
         updateText();
     }
 }
-function hireMiner(num){
+function hireMiner(num: number) {
     if (resources["gold"] >= costs["miner"] * num){
         if (!resources["miner"]){
             resources["miner"] = 0;
@@ -52,30 +52,30 @@ function hireMiner(num){
         }
         resources["miner"] += num;
         resources["gold"] -= num * costs["miner"];
-    
+
         costs["miner"] *= growthRate["miner"];
-    
+
         updateText();
 
-    
+
     }
 }
 
 /* exported mineGold upgradeMinerPickaxe upgradePickaxe hireMiner */
 
-function updateText(){
+function updateText() {
     for (const key in unlocks){
         let unlocked = true;
         for (const criterion in unlocks[key]){
             unlocked = unlocked && resources[criterion] >= unlocks[key][criterion];
         }
         if (unlocked){
-            for (const element of document.getElementsByClassName("show_" + key)){        
+            for (const element of document.getElementsByClassName("show_" + key)){
                 element.style.display = "block";
             }
         }
     }
-    
+
     for (const key in resources){
         for (const element of document.getElementsByClassName(key)){
             element.innerHTML = resources[key].toFixed(2);
@@ -89,26 +89,25 @@ function updateText(){
 }
 
 
-window.setInterval(function(){
+window.setInterval(() => {
     timer += tickRate;
 
-    
     for (const increment of increments){
         let total = 1;
         for (const input of increment["input"]){
             total *= resources[input];
-        
+
         }
         if (total){
-            console.log(total);
+
             resources[increment["output"]] += total / tickRate;
         }
     }
-    
+
     if (timer > visualRate){
         timer -= visualRate;
         updateText();
     }
-  
+
 
 }, tickRate);
