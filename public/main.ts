3ef11e91f4dcc0e521f3927d35fda4b08714c3f5
JamesGaier <33331904+JamesGaier@users.noreply.github.com>
2020-09-02 11:22:23 -0700

    Added GUI + functionality (#11)
    * first commit
    
    * removed unneeded code
    
    * added gui elements
    
    * added functionality to gui
    
    * Delete main.js
    
    This file is an output of `tsc` and should not be needed here.
    
    * linted code
    
    * switched to setInnerHTML function
    
    * fixed setInnerHTML function
    
    * fixed linting warnings and fixed negative crop bug
    
    * removed two lines of code...
    
    * removed two lines of code...
    
    * Update public/main.ts
    
    Co-authored-by: Nicholas Baron <nicholas.baron.ten@gmail.com>
    
    Co-authored-by: Nicholas Baron <nicholas.baron.ten@gmail.com>

--- public/main.ts
+++ public/main.ts
@@ -132,6 +132,11 @@ const kingdoms = [
 const player = kingdoms[0];
 player.farms.set(Crop.Rye, new Farm(getRandInt(minStartingRye, maxStartingRye)));
 
+// indicies for select fields
+let selectedFarm = "Rye";
+// let selectedCrop = "Rye";
+// let sellAmount = "";
+
 function promptPlayer(message: string): string {
     let result = null;
     while(result == null)
@@ -139,20 +144,179 @@ function promptPlayer(message: string): string {
     return result;
 }
 
-function updateText() {
-
+interface Printable { toString(): string }
+type tag = HTMLElement | HTMLInputElement;
+function setElementInnerHTML(el: tag, content: Printable) {
+    el.innerHTML = content.toString();
+}
+function setIdInnerHTML(id: string, content: Printable) {
+    setElementInnerHTML(document.getElementById(id)!, content);
 }
 
 
-window.setInterval(() => {
-    timer += tickRate;
+function handleBuy(ev: MouseEvent) {
+    const element = ev.target as HTMLElement;
+    const name = element.id;
+    const key = name.substring(0, name.indexOf("-"));
+    const crop = Crop[key as keyof typeof Crop];
 
+    if(player.gold >= cropMarket.get(crop)!) {
 
+        if(!player.farms.has(crop)) {
+            player.farms.set(crop, new Farm(1));
+            const stats = document.getElementById("gold-grain-stats");
+            const grainStat = document.createElement("p");
+            grainStat.id = Crop[crop] + "-stats";
+            setElementInnerHTML(grainStat, player.farms.get(crop)!.stockpile.toString() + " " + Crop[crop]);
 
-    if (timer > visualRate){
-        timer -= visualRate;
-        updateText();
+            const farms = document.getElementById("farms");
+            const farmOption = document.createElement("option");
+            farmOption.id = Crop[crop] + "-farm-option";
+            setElementInnerHTML(farmOption, Crop[crop]);
+            farms!.appendChild(farmOption);
+
+            const sellCrops = document.getElementById("crops-sale");
+            const cropOption = document.createElement("option");
+            cropOption.id = Crop[crop] + "-crop-option";
+            setElementInnerHTML(cropOption, Crop[crop]);
+            sellCrops!.appendChild(cropOption);
+
+            stats?.appendChild(grainStat);
+        }
+        else player.farms.get(crop)!.stockpile += 1;
+
+        player.gold -= cropMarket.get(crop)!;
     }
 
 
-}, tickRate);
+}
+
+// load gui
+function loadGUI() {
+    cropMarket.forEach((value, key) => {
+        const button = document.createElement("button");
+        button.onclick = handleBuy;
+        button.id = Crop[key] + "-store";
+        setElementInnerHTML(button, Crop[key] + " " + value.toString() + "G");
+        const store = document.getElementById("store-tab");
+        store?.appendChild(button);
+    });
+
+    setIdInnerHTML("gold", player.gold.toString());
+
+    player.farms.forEach((value: Farm, key: Crop) => {
+        const parent = document.getElementById("gold-grain-stats");
+        const grain = document.createElement("p");
+        grain.id = Crop[key] + "-stats";
+        setElementInnerHTML(grain, value.stockpile + " " + Crop[key]);
+        parent!.appendChild(grain);
+
+        const farms = document.getElementById("farms");
+        const farmOption = document.createElement("option");
+        farmOption.id = Crop[key] + "-farm-option";
+        setElementInnerHTML(farmOption, Crop[key]);
+        farms!.appendChild(farmOption);
+
+        const sellCrops = document.getElementById("crops-sale");
+        const cropOption = document.createElement("option");
+        cropOption.id = Crop[key] + "-crop-option";
+        setElementInnerHTML(cropOption, Crop[key]);
+        sellCrops!.appendChild(cropOption);
+    });
+
+    setIdInnerHTML("people", player.idlePopulation.toString());
+    setIdInnerHTML("soldiers", player.army.totalSoldiers.toString());
+    setIdInnerHTML("farmers", getTotalFarmers().toString());
+}
+function getTotalFarmers() {
+    let totalFarmers = 0;
+    player.farms.forEach((value: Farm) => {
+        totalFarmers += value.totalFarmers;
+    });
+    return totalFarmers;
+}
+function updateStats() {
+    setIdInnerHTML("gold", player.gold.toString());
+    setIdInnerHTML("soldiers", player.army.totalSoldiers.toString());
+    setIdInnerHTML("farmers", getTotalFarmers().toString());
+    setIdInnerHTML("people", player.idlePopulation.toString());
+    player.farms.forEach((value: Farm, key: Crop) => {
+        setIdInnerHTML(Crop[key] + "-stats", value.stockpile.toString() + " " + Crop[key]);
+        setIdInnerHTML(Crop[key] + "-farm-option", Crop[key]);
+        setIdInnerHTML(Crop[key] + "-crop-option", Crop[key]);
+    });
+
+}
+// function handleHarvest() {
+
+// }
+function handleAssignFarmer() {
+    if(player.idlePopulation > 0){
+        player.idlePopulation--;
+        player.farms.get(Crop[selectedFarm as keyof typeof Crop])!.totalFarmers++;
+    }
+}
+function handleRemoveFarmer() {
+    if(player.farms.get(Crop[selectedFarm as keyof typeof Crop])!.totalFarmers > 0) {
+        player.farms.get(Crop[selectedFarm as keyof typeof Crop])!.totalFarmers--;
+        player.idlePopulation++;
+    }
+}
+function handleAssignSoldier() {
+    if(player.idlePopulation > 0) {
+        player.idlePopulation--;
+        player.army.totalSoldiers++;
+    }
+}
+function handleRemoveSoldier() {
+    if(player.army.totalSoldiers > 0) {
+        player.idlePopulation++;
+        player.army.totalSoldiers--;
+    }
+}
+function handleFarmChoice(element: HTMLInputElement) {
+    selectedFarm = element.value;
+}
+// function handleCropChoice(element: HTMLInputElement) {
+//     selectedCrop = element.value;
+// }
+// function handleSellChoice(element: HTMLInputElement) {
+
+// }
+// function handleSellAmount(element: HTMLInputElement) {
+//     sellAmount = element.value;
+// }
+function sellCrop() {
+    try {
+        const sellAmount = document.getElementById("amount") as HTMLInputElement;
+        const amount = sellAmount.value;
+
+        const cropElement = document.getElementById("crops-sale") as HTMLInputElement;
+        const selectedCrop = cropElement.value;
+        console.log(selectedCrop);
+
+        if(isNaN(Number(amount))) return;
+        const crop = Crop[selectedCrop as keyof typeof Crop];
+        if(player.farms.get(crop)!.stockpile > 0 && parseInt(amount) <= player.farms.get(crop)!.stockpile) {
+            player.gold += parseInt(amount) * cropMarket.get(crop)!;
+            player.farms.get(crop)!.stockpile -= parseInt(amount);
+        }
+    } catch(err) {
+        console.error(err);
+    }
+}
+window.onload = () => {
+    window.setInterval(() => {
+        timer += tickRate;
+
+
+
+        if (timer > visualRate){
+            timer -= visualRate;
+            updateStats();
+        }
+
+
+    }, tickRate);
+    loadGUI();
+};
